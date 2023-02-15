94a23f66824981e363f0de416abf7b08ab1e6479
JigarRajput <jigar.r@simformsolutions.com>
2023-02-15 13:14:06 +0530

    c m 4

--- login.js
+++ login.js
@@ -1,5 +1,9 @@
 // jai Ganesh
+<<<<<<< HEAD
 var login = "edited"// new change
 var one = null
 var two = null
 var three
+=======
+var login = "edited"// new change
+>>>>>>> parent of 8319afc (Revert "added : login functionality updated")
